118d2efdea494447b80e345984d62cbf0ff800be
purohitBhaskar <bhaskarp@designbytes.org>
2024-03-27 13:59:20 +0530

    routes

--- app.js
+++ app.js
@@ -9,8 +9,103 @@ const app = express()
 app.use(cors())
 
 
-app.get('')
+app.get('/voltage',
+    (req,res)=>{
+        try{
+             res.status(200).json({
+                message:{
+                    title: 'getting voltage values',
+                    detail:12
+                }
+            })
+        }catch(err){
+            console.error(err)
+        }
+    }
 
+)
+
+app.get('/current',
+    (req,res)=>{
+        res.status(200).json({
+            message:{
+                title: 'getting current values',
+                detail:120
+            }
+        })
+    }
+)
+
+app.get('/frequency',
+    (req,res)=>{
+        res.status(200).json({
+            title: 'getting frequency values',
+            detail: 49.5
+        })
+    }
+)
+
+app.get('/power',
+   (req,res)=>{
+    res.status(200).json({
+        message:{
+            title: 'getting power values',
+            detail: 44
+        }
+    })
+   }
+
+)
+
+app.get('/powerinKVA',(req, res)=>{
+    res.status.json({
+        message:{
+            title: 'getting power values in KVA',
+            detail: 44
+        }
+    })
+})
+
+app.get('/engine-running-hrs',(req, res)=>{
+    res.status(200).json({
+        message:{
+            title: 'getting engine running status values',
+            detail: 10
+        }
+    })
+})
+
+app.get('/engine-rpm',(req,res)=>{
+    res.status(200).json({
+        message:{
+            title: 'getting engine RPM values',
+            detail: 50
+        }
+    })
+})
+
+
+app.get('/coolant-temperature',(req,res)=>{
+    res.status(200).json({
+        message:{
+            title: 'getting coolant temperature',
+            detail: 50
+        }
+    })
+})
+
+app.get('/oil-pressure', (req,res)=>{
+    res.status(200).json({
+        message:{
+            title: 'getting oil pressure',
+            detail: 50
+        }
+    })
+})
+
+app.get('/engine/:id/voltage',(req,res)=>{
+    res.status(200)
+})
 
 app.listen(port,()=>{
     console.log('server running ');
